6ac06bbdc1cc0758dd68d239d5d497404d7df2af
shawndcarpenter <shawncarpenter.co@gmail.com>
2024-07-04 15:28:13 -0600

    Add skills to welcome page

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,5 @@
 class WelcomeController < ApplicationController
   def index
-    
+    @skills = Skill.all
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,34 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+skills = [
+  "SQL",
+  "APIs",
+  "ActiveRecord",
+  "Agile Workflow",
+  "REST",
+  "GraphQL",
+  "Ruby",
+  "Ruby on Rails",
+  "Python",
+  "HTML",
+  "C",
+  "CSS",
+  "Git",
+  "GitHub",
+  "Heroku Deployment",
+  "Postman",
+  "DigitalOcean",
+  "BootStrap",
+  "MVC",
+  "OAuth",
+  "Unit Testing",
+  "Integration Testing",
+  "CSV parsing",
+  "JavaScript"
+]
+
+# Create skills records
+skills.each do |skill|
+  Skill.create(text: skill)
+end
